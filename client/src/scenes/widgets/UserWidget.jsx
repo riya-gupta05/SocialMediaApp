91e0fc99968dc9c617b333d85ae021a83782bce9
Riya <97406276+riya-gupta05@users.noreply.github.com>
2024-01-01 20:16:23 +0530

    Update UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -11,7 +11,7 @@ import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {URL} from "baseURL";
+import {URL} from "baseUrl";
 
 const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
