fff905b63153f13525f3c59d31c1c74d631eecf7
Riya <97406276+riya-gupta05@users.noreply.github.com>
2024-01-01 18:37:52 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import {URL} from "baseURL";
 
 const UserImage = ({ image, size = "60px" }) => {
   return (
@@ -8,7 +9,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`${URL}/assets/${image}`}
       />
     </Box>
   );
